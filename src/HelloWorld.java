d4dfce3dd47f7b861d81517df9b630d55cb6ced3
KunYoung Heo <notegrapher@gmail.com>
2017-06-29 14:59:39 +0900

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	public static void main(String [] args){
 		System.out.println("Hello Java!");
-		// System.out.println("Programming Study");
+		System.out.println("Programming Study");
+		System.out.println("단축키");
 	}
 }
