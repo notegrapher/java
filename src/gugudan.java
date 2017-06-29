d30ba05cb8e5e1459d9f71f10f12b932d60d305d
KunYoung Heo <notegrapher@gmail.com>
2017-06-29 15:32:58 +0900

    gugudan version 2.1

--- src/gugudan.java
+++ src/gugudan.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class gugudan {
 	public static void main(String[] args){
-		System.out.print("구구단 출려기, 원하는 단을 입력해주세요 : ");
+		System.out.print("구구단 출력기, 원하는 단을 입력해주세요 : ");
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
 		int result = number * 1;
