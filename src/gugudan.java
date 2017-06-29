c209be6430b2d90d91a0f54f29039ae8d1456cda
KunYoung Heo <notegrapher@gmail.com>
2017-06-29 15:32:31 +0900

    gugudan version 2.0

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,25 +1,12 @@
+import java.util.Scanner;
 
 public class gugudan {
 	public static void main(String[] args){
-		System.out.println("2 * 1 = 2");
-		System.out.println("2 * 2 = 4");
-		System.out.println("2 * 3 = 6");
-		System.out.println("2 * 4 = 8");
-		System.out.println("2 * 5 = 10");
-		System.out.println("2 * 6 = 12");
-		System.out.println("2 * 7 = 14");
-		System.out.println("2 * 8 = 16");
-		System.out.println("2 * 9 = 18");
-		
-		System.out.println("3 * 1 = 3");
-		System.out.println("3 * 2 = 6");
-		System.out.println("3 * 3 = 9");
-		System.out.println("3 * 4 = 12");
-		System.out.println("3 * 5 = 15");
-		System.out.println("3 * 6 = 18");
-		System.out.println("3 * 7 = 21");
-		System.out.println("3 * 8 = 24");
-		System.out.println("3 * 9 = 27");
+		System.out.print("구구단 출려기, 원하는 단을 입력해주세요 : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		int result = number * 1;
+		System.out.println(result);
 		
 	}
 }
