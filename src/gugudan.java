4fbe9e444a72791718c30ac648389a6683f761a3
KunYoung Heo <notegrapher@gmail.com>
2017-06-30 13:25:11 +0900

    gugudan version 2.6(array)

--- src/gugudan.java
+++ src/gugudan.java
@@ -4,18 +4,24 @@ public class gugudan {
 	public static void main(String[] args){
 		System.out.print("구구단 출력기, 원하는 단을 입력해주세요 : ");
 		Scanner scanner = new Scanner(System.in);
-		int i;
+		int i, SIZE = 9;
+		int multi[] = new int[SIZE];
+		
 		int number = scanner.nextInt();
 //		System.out.println("for문을 통한 구구단 출력");
 		if(number >= 2 && number < 9){
 			for(i=1; i<10; i++){
 				int result = number * i;
-				System.out.println(result);	
+				multi[i-1] = result;
 			}
 		}
-		
+				
 		else{
 			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+		}
+		
+		for(i=0; i<multi.length; i++){
+			System.out.println(multi[i]);
 		}
 //		i = 1;
 //		System.out.println("while문을 통한 구구단 출력");
